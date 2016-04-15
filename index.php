79afd1670b048e398089a4bd8fb1af489797931d
BobbieO <bobbie2504@msn.com>
2016-04-15 16:00:33 -0500

    Added voices

--- index.php
+++ index.php
@@ -1,32 +1,41 @@
 <?php
 
+`say -v Zarvox Shall, we, play, a, game`;
 
 //game generates random number
 $gameNum = mt_rand (1, 100);
 echo "The computer is secretly thinking {$gameNum}\n";
 
+//pause for voice
+sleep (2);
+
 // game prompts user for her guess
 fwrite(STDOUT, "I'm thinking of a number from 1-100. What is it?\n");
+`say -v Zarvox I am thinking of a number from 1 to 100, What is it`;
 
 //counts guesses
 $guessAttempts = 0;
 
 do {
-
     $userGuess = trim(fgets(STDIN));
-
+   
     if ($userGuess > $gameNum) {
         fwrite(STDOUT, "Nope, too high. Try again!\n");
+        `say -v Hysterical Nope, too high, Try again`;
         $guessAttempts++;
     } elseif ($userGuess < $gameNum) {
         fwrite(STDOUT, "Nope, too low. Try again!\n");
+        `say -v Hysterical Nope, too low, Try again`;
         $guessAttempts++;
     } 
 
 } while ($userGuess != $gameNum);
 
 fwrite(STDOUT, "You won! {$gameNum} was my number!\n");
+`say -v Bad News You won, {$gameNum} was my number`;
+
 fwrite(STDOUT, "It took {$guessAttempts} times to read my mind.\n");
+`say -v Zarvox It took {$guessAttempts} times to read my mind`;
 
 
 
