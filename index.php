5cd636c16750f58ee0d02d76468448de0f0e9157
BobbieO <bobbie2504@msn.com>
2016-04-15 15:15:50 -0500

    Added guess counter to keep track of guesses

--- index.php
+++ index.php
@@ -8,6 +8,8 @@ echo "The computer is secretly thinking {$gameNum}\n";
 // game prompts user for her guess
 fwrite(STDOUT, "I'm thinking of a number from 1-100. What is it?\n");
 
+//counts guesses
+$guessAttempts = 0;
 
 do {
 
@@ -15,13 +17,16 @@ do {
 
     if ($userGuess > $gameNum) {
         fwrite(STDOUT, "Nope, too high. Try again!\n");
+        $guessAttempts++;
     } elseif ($userGuess < $gameNum) {
         fwrite(STDOUT, "Nope, too low. Try again!\n");
+        $guessAttempts++;
     } 
-    
+
 } while ($userGuess != $gameNum);
 
 fwrite(STDOUT, "You won! {$gameNum} was my number!\n");
+fwrite(STDOUT, "It took {$guessAttempts} times to read my mind.\n");
 
 
 
